1c176525011034a2cdc9759ec089b890dd6efbf0
fionayzhao <fiona_zhao212@yahoo.com>
2014-11-18 16:21:03 -0800

    calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, total;
+
+	cout << "Enter the integers to be added:" << endl;
+	cin >> a >> b;
+	total = a + b;
+	cout << "The sum is" << total << endl;
+
+	return 0;
+}
